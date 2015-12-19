b757da2bbaf9e37b59f7fe0823b37cd0285f0fc4
Melnick <melnick.git@ya.ru>
2015-12-19 04:42:07 +0300

    Bad memory

--- main.py
+++ main.py
@@ -1,108 +1,41 @@
-# from unicurses import *
-from time import *
-from random import *
+import sys
+import traceback
 
-from cnake import *
-from food import *
-# from functions import *s
+from core.game import *
+from core.functions import *
 
 # Coords format - (y, x)
-START_COORDS = (2, 2);
+
 
 
 def main():
-	stdscr = initscr()
+	try:
+		game_init();
+	except Exception as e:
+		exc_type, exc_value, exc_traceback = sys.exc_info();
+		tb = traceback.format_exception(exc_type, exc_value, exc_traceback);
+		string = ''.join(tb);
+		w_log("last.log", "w", "game_init\n" + string + "\n\n");
+		# w_log("logs.log", "a+", "{}\n\n".format(s));
 
-	screen_resolution = [(0, 0), getmaxyx(stdscr)];
-	start_coords = get_mid( screen_resolution, (-5, -30) );
 
-	# mvaddstr(6, 5, screen_resolution )
-
-
-	noecho()
-	curs_set(0)
-	keypad(stdscr, True)
-	nodelay(stdscr, True)
-
-	border()
-
-	cnake = Cnake( start_coords );
-	food = Food( screen_resolution );
-	food.spawn();
-	render( food.food );
-
+	# End
 	while ( True ):
-		cnake.del_dead_tail();
-
-
-		for i in range( len(cnake.body) ):
-			render( [cnake.TAIL_CH, (cnake.body[i][0], cnake.body[i][1])] );
-		render( [cnake.HEAD_CH, (cnake.head[0], cnake.head[1])] );
-
-
-
 		c = getch()
-
-		if   ( c == KEY_LEFT  and not cnake.to_vector('r') ):
-			cnake.vector = cnake.direction['l'];
-
-		elif ( c == KEY_RIGHT and not cnake.to_vector('l') ):
-			cnake.vector = cnake.direction['r'];
-
-		elif ( c == KEY_UP    and not cnake.to_vector('b') ):
-			cnake.vector = cnake.direction['t'];
-
-		elif ( c == KEY_DOWN  and not cnake.to_vector('t') ):
-			cnake.vector = cnake.direction['b'];
-
-
-		cnake.move();
-
-		p = mvinch(cnake.head[0], cnake.head[1]);
-		if ( p != ord(' ') and chr(p) in Food.foods):
-			food.spawn();
-			render( food.food );
-			cnake.add_body();
-		elif ( p != ord(' ') ):
-			cnake.dead = True;
-
-
-		if ( cnake.dead ):
+		if ( c == KEY_ESC ):
+			clear();
+			refresh();
 			break;
-
-		refresh()
-		sleep(0.05)
-
-	mvaddstr(6, 5, food.food)
+	endwin()
 
 
-
-
-
-
-
-
-try:
+if (__name__ == "__main__"):
 	main();
-except Exception as e:
-	clear();
-	refresh();
-	print( "{0}".format(e) );
 
-	f = open("last.log", "w");
-	f.write( "{0}".format(e) );
-	f.close();
-
-	f = open("all.log", "a+");
-	f.write( "{0}\n\n".format(e) );
-	f.close();
-
-
-# End
-while ( True ):
-	c = getch()
-	if ( c == ord('q') ):
-		clear();
-		refresh();
-		break;
-endwin()
+	# try:
+	# 	main();
+	# except Exception as e:
+	# 	exc_type, exc_value, exc_traceback = sys.exc_info();
+	# 	tb = traceback.format_exception(exc_type, exc_value, exc_traceback);
+	# 	string = ''.join(tb);
+	# 	w_log("last.log", "a+", 'main\n' + string + "\n\n");
